ad3203e4ea2c25998b8794664158ada2737dfade
Brayan-Restrepo <brayan2293@gmail.com>
2018-08-15 22:56:30 -0500

    ngForm

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '../../../../node_modules/@angular/forms';
 
 @Component({
   selector: 'app-template',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateComponent implements OnInit {
 
+  public usuario: Object = {
+    nombre: 'Brayan',
+    apellido: 'Restrepo',
+    correo: ''
+  };
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  guardar(forma: NgForm) {
+    console.log(forma);
+    console.log(this.usuario);
+    console.log(forma.value);
+    console.log(forma.valid);
+  }
 }
